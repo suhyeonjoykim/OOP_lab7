a6e6e915d8737885689d66dbdc7a302af8c7230c
SuhyeonKim <kshyun0420@gmail.com>
2017-04-27 11:33:10 +0900

    Product modify

--- OOP_Lab7/src/Product.java
+++ OOP_Lab7/src/Product.java
@@ -1,4 +1,17 @@
 
-public class Product {
+public abstract class Product {
 
+	public double price;
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public String getName(String name){
+		return name;
+	}
 }
